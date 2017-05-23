1f0e35fb15ce57bc49363eadfa06a70b32c84cc9
Marco Vermeulen <vermeulen.mp@gmail.com>
2017-05-23 05:35:15 +0200

    Add integration test for version repo save function.

--- src/it/scala/io/sdkman/vendor/release/repos/VersionsRepoSpec.scala
+++ src/it/scala/io/sdkman/vendor/release/repos/VersionsRepoSpec.scala
@@ -1,23 +1,31 @@
 package io.sdkman.vendor.release.repos
 
-import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.concurrent.{Eventually, IntegrationPatience, ScalaFutures}
 import org.scalatest.{BeforeAndAfter, Matchers, OptionValues, WordSpec}
 import support.Mongo
 
-class VersionsRepoSpec extends WordSpec with Matchers with BeforeAndAfter with ScalaFutures with OptionValues {
+class VersionsRepoSpec extends WordSpec with Matchers with BeforeAndAfter with ScalaFutures with OptionValues with Eventually with IntegrationPatience {
 
   val repo = new VersionsRepo {}
 
+  val candidate = "java"
+  val version = "8u111"
+  val platform = "LINUX_64"
+  val url = "http://dl/8u111-b14/jdk-8u111-linux-x64.tar.gz"
+
   "versions repository" should {
+
+    "persist a version" in {
+      repo.saveVersion(Version(candidate, version, platform, url))
+
+      eventually {
+        Mongo.versionPublished(candidate, version, url, platform) shouldBe true
+      }
+    }
 
     "attempt to find one Version by candidate, version and platform" when {
 
       "that version is available" in {
-        val candidate = "java"
-        val version = "8u111"
-        val platform = "LINUX_64"
-        val url = "http://dl/8u111-b14/jdk-8u111-linux-x64.tar.gz"
-
         Mongo.insertVersion(Version(candidate, version, platform, url))
 
         whenReady(repo.findVersion(candidate, version, platform)) { maybeVersion =>
