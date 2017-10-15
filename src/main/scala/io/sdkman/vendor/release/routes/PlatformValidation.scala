6c70a1c6091f933131422702624c3af43452fd3c
Marco Vermeulen <vermeulen.mp@gmail.com>
2017-10-15 18:13:49 +0100

    Add support for 32-bit Linux.

--- src/main/scala/io/sdkman/vendor/release/routes/PlatformValidation.scala
+++ src/main/scala/io/sdkman/vendor/release/routes/PlatformValidation.scala
@@ -6,7 +6,7 @@ trait PlatformValidation {
 
   self: Directives =>
 
-  val SupportedPlatforms = Seq("LINUX_64", "MAC_OSX", "WINDOWS_64", "UNIVERSAL")
+  val SupportedPlatforms = Seq("LINUX_64", "LINUX_32", "MAC_OSX", "WINDOWS_64", "UNIVERSAL")
 
   def validatePlatform(platform: String): Directive0 =
     validate(SupportedPlatforms.contains(platform), s"Invalid platform: $platform")
