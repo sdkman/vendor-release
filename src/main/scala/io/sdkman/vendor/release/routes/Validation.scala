8a81204222fba3edb523c793a045a465fd7b1040
Marco Vermeulen <vermeulen.mp@gmail.com>
2018-06-12 12:56:19 +0100

    Remove obsolete case class.

--- src/main/scala/io/sdkman/vendor/release/routes/Validation.scala
+++ src/main/scala/io/sdkman/vendor/release/routes/Validation.scala
@@ -10,7 +10,6 @@ trait Validation {
   import ApiResponseJsonProtocol._
   import spray.json._
 
-
   val SupportedPlatforms = Seq("LINUX_64", "LINUX_32", "MAC_OSX", "WINDOWS_64", "UNIVERSAL")
 
   def validatePlatform(platform: String): Directive0 =
@@ -19,5 +18,4 @@ trait Validation {
   def validateUrl(url: String): Directive0 =
     validate(resourceAvailable(url), ApiResponse(400, s"URL cannot be resolved: $url").toJson.compactPrint)
 
-  case class Error(code: Int, message: String)
 }
