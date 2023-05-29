742d12d077734abc0e8a706e6f9e399539bd5a90
Marco Vermeulen <vermeulen.mp@gmail.com>
2023-05-29 21:41:51 +0100

    Add candidate table.

--- src/main/resources/db/migration/V1__create_version_table.sql
+++ src/main/resources/db/migration/V1__create_version_table.sql
@@ -1,9 +1,10 @@
-CREATE TABLE version(
-    id SERIAL PRIMARY KEY,
+CREATE TABLE version
+(
+    id        SERIAL PRIMARY KEY,
     candidate TEXT,
-    version TEXT,
-    platform TEXT,
-    visible BOOLEAN,
-    url TEXT,
+    version   TEXT,
+    platform  TEXT,
+    visible   BOOLEAN,
+    url       TEXT,
     UNIQUE (candidate, version, platform)
 )
---
+++ src/main/resources/db/migration/V2__create_candidate_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE candidate
+(
+    id              TEXT PRIMARY KEY,
+    name            TEXT,
+    description     TEXT,
+    default_version TEXT,
+    website_url     TEXT,
+    distribution    TEXT
+)
